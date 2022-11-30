29b380d2092a1d61eaeb61c1f4bdb40dd9e374ed
Your Name <you@example.com>
2022-11-30 14:58:19 +0600

    add templates

--- djblog/blog/views.py
+++ djblog/blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+menu = ['О сайте', 'Добавить статью', 'Обратная связь']
+
+
 def index(request):
-    return render(request,'blog/index.html')
+    return render(request, 'blog/index.html', {'title': 'index', 'menu': menu})
+
+
+def about(request):
+    return render(request, 'blog/about.html')
